876561185a26676ef4c6d9b338384ebd4e5faf60
fwfly <fawafwfly@gmail.com>
2020-04-26 16:30:01 +1000

    Fix several items
    It can be tested over 137
    
    1. Fix fib incorrect answer
       Fix misunderstanding part of original source.
    
    2. Original source may be incorrect code.
       Add this following line can fix that
          line out->lower += out->upper;
    
    3. Correct  ULONG_MAX to ULLONG_MAX

--- fibdrv.c
+++ fibdrv.c
@@ -33,101 +33,15 @@ struct BigN {
     unsigned long long lower, upper;
 };
 
-static inline void addBigN(struct BigN *out, struct BigN x, struct BigN y)
-{
-    out->upper = x.upper + y.upper;
-    unsigned long long lower = ~x.lower;
-    if (y.lower > lower) {
-        out->upper++;
-    }
-    out->lower = x.lower + y.lower;
-}
-
-
-/* Here is an assumption is x is always bigger then y */
-static inline void subBigN(struct BigN *out, struct BigN x, struct BigN y)
-{
-    out->upper = x.upper - y.upper;
-    if (y.lower > x.lower) {
-        x.upper--;
-        out->lower = ULONG_MAX - y.lower + x.lower;
-    } else {
-        out->lower = x.lower - y.lower;
-    }
-    // printk("%lld = %lld - %lld", out->lower, x.lower, y.lower);
-}
-
-/* Multi is loop addBigN y times */
-/*static inline void multiBigN(struct BigN *out, struct BigN x, struct BigN y)
-{
-
-    out->upper = 0;
-    out->lower = 0;
-    // unsigned long long tmpx = x.lower, tmpy = y.lower;
-
-    if (!y.upper && y.lower >= 1) {
-        out->upper = x.upper;
-        out->lower = x.lower;
-    }
-
-    while ((y.upper != 0) || (y.lower > 1)) {
-        while (y.lower > 1) {
-            addBigN(out, *out, x);
-            y.lower--;
-        }
-        if (y.upper) {
-            y.upper--;
-            y.lower = ULONG_MAX;
-        }
-    }
-    // printk("Mult %lld = %lld * %lld", out->lower, tmpx, tmpy);
-}*/
-
-
-static inline void multiBigN64to128(struct BigN *out,
-                                    unsigned long long x,
-                                    unsigned long long y)
-{
-    uint32_t a = x & 0xFFFFFFFF;
-    uint32_t c = x >> 32;
-    uint32_t b = y & 0xFFFFFFFF;
-    uint32_t d = y >> 32;
-    uint64_t ab = (uint64_t) a * b;
-    uint64_t bc = (uint64_t) b * c;
-    uint64_t ad = (uint64_t) a * d;
-    uint64_t cd = (uint64_t) c * d;
-    uint64_t low = ab + (bc << 32);
-    uint64_t high = cd + (bc >> 32) + (ad >> 32) + (low < ab);
-    low += (ad << 32);
-    high += (low < (ad << 32));
-    out->lower = low;
-    out->upper = high;
-}
-
-static inline void multiBigN(struct BigN *out, struct BigN x, struct BigN y)
-{
-    out->upper = 0;
-    out->lower = 0;
-    unsigned long long h = x.lower * y.upper + x.upper * y.lower;
-    multiBigN64to128(out, x.lower, y.lower);
-    x.upper += h;
-    out->upper = x.upper;
-}
-
-static inline void assignBigN(struct BigN *x, struct BigN y)
-{
-    x->upper = y.upper;
-    x->lower = y.lower;
-}
 
 // To pass the clang-format, we comment this code.
 // This function is useful when we need to verify the number to human-readable.
-/*void BigN_to_int(struct BigN *res, struct BigN x)
+/* void BigN_to_int(struct BigN *res, struct BigN x)
 {
     unsigned long long max10 = 10000000000000000000U;
     unsigned long long idx = x.upper;
-    unsigned long long max_first = ULONG_MAX / max10;
-    unsigned long long max_mod = ULONG_MAX - max_first * max10;
+    unsigned long long max_first = ULLONG_MAX / max10;
+    unsigned long long max_mod = ULLONG_MAX - max_first * max10;
 
     res->lower = x.lower;
     unsigned long long x_first = x.lower / max10;
@@ -148,7 +62,89 @@ static inline void assignBigN(struct BigN *x, struct BigN y)
         res->upper = x_first;
         idx--;
     }
-}*/
+} */
+
+
+
+static inline void addBigN(struct BigN *out, struct BigN x, struct BigN y)
+{
+    out->upper = x.upper + y.upper;
+    unsigned long long lower = ~x.lower;
+    if (y.lower > lower) {
+        out->upper++;
+    }
+    out->lower = x.lower + y.lower;
+}
+
+
+/* Here is an assumption is x is always bigger then y */
+static inline void subBigN(struct BigN *out, struct BigN x, struct BigN y)
+{
+    out->upper = x.upper - y.upper;
+    if (y.lower > x.lower) {
+        x.upper--;
+        out->lower = ULLONG_MAX - y.lower + x.lower;
+    } else {
+        out->lower = x.lower - y.lower;
+    }
+}
+
+
+static inline void multiBigN64to128(struct BigN *out,
+                                    unsigned long long x,
+                                    unsigned long long y)
+{
+    uint32_t a = x & 0xFFFFFFFF;
+    uint32_t c = x >> 32;
+    uint32_t b = y & 0xFFFFFFFF;
+    uint32_t d = y >> 32;
+    // printk("  %u  %u", c, a);
+    // printk("x %u  %u", d, b);
+    // printk("-------------");
+
+    uint64_t ab = (uint64_t) a * b;
+    uint64_t bc = (uint64_t) b * c;
+    uint64_t ad = (uint64_t) a * d;
+    uint64_t cd = (uint64_t) c * d;
+    // printk("ab: %llu\nbc: %llu\nad: %llu\ncd: %llu", ab, bc, ad, cd );
+
+    uint64_t low = ab + (bc << 32);
+    // printk("Low : %llu", low);
+    uint64_t high = cd + (bc >> 32) + (ad >> 32) + (low < ab);
+    low += (ad << 32);
+    // printk("Low: %llu", low);
+    high += (low < (ad << 32));
+    out->lower = low;
+    out->upper = high;
+}
+
+static inline void multiBigN(struct BigN *out, struct BigN x, struct BigN y)
+{
+    out->upper = 0;
+    out->lower = 0;
+    unsigned long long h = x.lower * y.upper + x.upper * y.lower;
+    multiBigN64to128(out, x.lower, y.lower);
+    out->upper += h;
+    out->lower += out->upper;
+
+
+    // printk("Mult %llu %llu = %llu * %llu",out->upper, out->lower, x.lower,
+    // y.lower);
+    /* struct BigN res;
+    res.upper = 0;
+    res.lower = 0;
+    BigN_to_int( &res, *out );
+    if (res.upper)
+        printk("%llu %llu = %llu * %llu", res.upper, res.lower, x.lower,
+    y.lower);
+    else printk("%llu = %llu * %llu",res.lower, x.lower, y.lower); */
+}
+
+static inline void assignBigN(struct BigN *x, struct BigN y)
+{
+    x->upper = y.upper;
+    x->lower = y.lower;
+}
 
 static int num_bits(long long k)
 {
@@ -180,38 +176,39 @@ static long long fast_fib_sequence(long long k)
     int count = numbits;
 
     while (count) {
-        /* t1 = a*(2*b - a) */
         multiBigN(&t1, b, b2);
         subBigN(&t1, t1, a);
         multiBigN(&t1, t1, a);
 
-        /* t2 = b^2 + a^2; */
         multiBigN(&t2, b, b);
         multiBigN(&t2a, a, a);
         addBigN(&t2, t2, t2a);
 
-        /* a = t1; b = t2; // m *= 2 */
         assignBigN(&a, t1);
         assignBigN(&b, t2);
-        // int tmpk = (k >> (count -1)) & 1;
-        // printk("%d ",tmpk );
         if ((k >> (count - 1)) & 1) {
-            // printk("%d : special count\n", tmpk);
             addBigN(&t1, a, b);  // t1 = a + b;
             assignBigN(&a, b);   // a = b;
             assignBigN(&b, t1);  // b = t1;
         }
+
         count--;
     }
-    /* struct BigN res;
+
+    /*if (a.upper)
+        printk("%llu: %llu %llu\n", k, a.upper, a.lower);
+    else
+        printk("%llu: %llu\n", k, a.lower);*/
+
+    /*struct BigN res;
     res.upper = 0;
     res.lower = 0;
     BigN_to_int( &res, a );
     if (res.upper)
-        printk("%lld: %lld %lld\n", k, res.upper, res.lower);
+        printk("%llu: %llu %llu\n", k, res.upper, res.lower);
     else
-        printk("%lld: %lld\n", k, res.lower); */
-    return a.lower;
+        printk("%llu: %llu\n", k, res.lower);
+    return a.lower;*/
 }
 
 
